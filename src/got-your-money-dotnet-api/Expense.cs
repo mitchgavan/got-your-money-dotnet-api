9bf5a43768c9f49cc35a8578dc31a157b768ca47
Mitch Gavan <mitchgavan@gmail.com>
2019-08-25 19:09:13 +1000

    update Expense JSON casing

--- src/got-your-money-dotnet-api/Expense.cs
+++ src/got-your-money-dotnet-api/Expense.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace got_your_money_dotnet_api
 {
   public class Expense
   {
+
+    [JsonProperty(PropertyName = "id")]
     public string Id { get; set; }
+    [JsonProperty(PropertyName = "name")]
     public string Name { get; set; }
+    [JsonProperty(PropertyName = "cost")]
     public double Cost { get; set; }
+    [JsonProperty(PropertyName = "purchaseDate")]
     public DateTime PurchaseDate { get; set; }
+    [JsonProperty(PropertyName = "createdTimestamp")]
     public DateTime CreatedTimestamp { get; set; }
   }
 }
